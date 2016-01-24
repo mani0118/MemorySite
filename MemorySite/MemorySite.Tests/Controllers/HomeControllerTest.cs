253b07dd795d64608bc8b4e77e60d90a0333ca0d
Mani <452217943@qq.com>
2016-01-24 21:53:48 +0800

    2016-1-24 21:53

--- MemorySite/MemorySite.Tests/Controllers/HomeControllerTest.cs
+++ MemorySite/MemorySite.Tests/Controllers/HomeControllerTest.cs
@@ -32,7 +32,7 @@ namespace MemorySite.Tests.Controllers
             HomeController controller = new HomeController();
 
             // Act
-            ViewResult result = controller.About() as ViewResult;
+            ViewResult result = controller.Heart() as ViewResult;
 
             // Assert
             Assert.AreEqual("Your application description page.", result.ViewBag.Message);
@@ -45,7 +45,7 @@ namespace MemorySite.Tests.Controllers
             HomeController controller = new HomeController();
 
             // Act
-            ViewResult result = controller.Contact() as ViewResult;
+            ViewResult result = controller.Index() as ViewResult;
 
             // Assert
             Assert.IsNotNull(result);
